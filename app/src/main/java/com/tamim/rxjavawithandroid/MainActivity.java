fbf4bc2f5ad75d33c4192c002839ea82c0bffc92
Tamim <tamimiskhan@gmail.com>
2021-09-22 14:05:58 +0600

    Initial Commit Rx java lession 3.

--- app/src/main/java/com/tamim/rxjavawithandroid/MainActivity.java
+++ app/src/main/java/com/tamim/rxjavawithandroid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -13,6 +14,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -65,6 +67,49 @@ public class MainActivity extends AppCompatActivity {
                 stockDataAdapter.add(stockUpdate);
             }
         });
+
+
+//        Observable.just("1")
+//                .map(new Function<String, String>() {
+//                    @Override
+//                    public String apply(String s) {
+//                        return s + "mapped";
+//                    }
+//                })
+//                .flatMap(new Function<String, Observable<String>>() {
+//                    @Override
+//                    public Observable<String> apply(String s) {
+//                        return Observable.just("flat-" + s);
+//                    }
+//                })
+//                .doOnNext(new Consumer<String>() {
+//                    @Override
+//                    public void accept(String s) {
+//                        Log.d("APP", "on next " + s);
+//                    }
+//                })
+//                .subscribe(new Consumer<String>() {
+//                    @Override
+//                    public void accept(String e) {
+//                        Log.d("APP", "Hello " + e);
+//                    }
+//                }, new Consumer<Throwable>() {
+//                    @Override
+//                    public void accept(Throwable throwable) {
+//                        Log.d("APP", "Error!");
+//                    }
+//                });
+
+
+        //That's 24 lines saved!
+
+        Observable.just("2")
+                .map(s -> s + "mapped")
+                .flatMap(s -> Observable.just("flat-" + s))
+                .doOnNext(s -> Log.d("APP", "on next " + s))
+                .subscribe(e -> Log.d("APP", "Hello " + e),
+                        throwable -> Log.d("APP", "Error!"));
+
 
 
     }
